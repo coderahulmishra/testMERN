d8fa0e152144c9c95f29c3c4cbeab68b318e62cb
coderahulmishra <coderahulmishra@gmail.com>
2025-03-13 14:07:16 +0530

    adding cors

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const authRouter = require("./router/auth_router");
 const MONGO_URI = "mongodb+srv://coderahulmishra:YYpRp6iZ1smjiWNU@cluster0.7s3lw.mongodb.net/firstDatabase"
+
+app.use(cors());
+
+app.use(cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:3000"], // Allow only these origins
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    credentials: true
+}));
+
 
 app.use(express.json());
 app.use("/auth/api",authRouter)
